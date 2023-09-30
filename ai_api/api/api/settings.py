8047f552968bb9231a8ef738c6de152952b55eb1
wiktorsk8 <wikto4k@gmail.com>
2023-09-30 20:31:40 +0200

    cors package added

--- ai_api/api/api/settings.py
+++ ai_api/api/api/settings.py
@@ -44,6 +44,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -122,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DEBUG=True
+
+CORS_ORIGIN_ALLOW_ALL = DEBUG
