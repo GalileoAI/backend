2ae9ff0d1b4cb70c387c23e25b1ac8f3c7920d90
kutasz <lukasz.janczak.pl@gmail.com>
2023-10-01 00:56:19 +0200

    Optimized request rate on gptClient

--- http_interface/http_interface.py
+++ http_interface/http_interface.py
@@ -14,35 +14,36 @@ class GPTClient:
         utils.save_message(self.messages[-1], initialized=True)
 
     def jobs_by_questionare(self, questionare) -> str:
-        self.send_prompt("system", f"Remember the answers given by the student on this questionare: {questionare}")
-        self.send_prompt("system", "You give a json string as an answer where every position has name and description")
+        self.save_message("system", f"Remember the answers given by the student on this questionare: {questionare}")
+        self.save_message("system", "You give a json string as an answer where every position has name and description")
         jobs = self.send_prompt("user", "What 5 jobs would be the most suitable for the student?")
         jobs.replace("\n", "")
         return jobs
 
     def schools_by_job(self, job):
-        self.send_prompt("system", f"Answer all questions based on this career path chosen by the student: {job}")
-        self.send_prompt("system", "You give a json string as an answer where every position has name and description")
+        self.save_message("system", f"Answer all questions based on this career path chosen by the student: {job}")
+        self.save_message("system", "You give a json string as an answer where every position has name and description")
         schools = self.send_prompt("user", "What are the 5 best universities in Poland that offer faculties related to this career path")
         return schools
 
+    def save_message(self, role, prompt):
+        message = {
+            "role": role,
+            "content": prompt
+        }
+
+        self.messages.append(message)
+        utils.save_message(message)
+
     def send_prompt(self, role, prompt):
-        self.messages.append(
-            {
-                "role": role,
-                "content": prompt
-            }
-        )
+        self.save_message(role, prompt)
 
         response = api_communication.post_gpt(self.model, self.messages)
 
         if response.ok:
             response_data = response.json()
             message = response_data["choices"][0]["message"]
-
-            self.messages.append(message)
-            utils.save_message(message)
-
+            self.save_message(message["role"], message["content"])
             return message["content"]
         else:
             raise Exception(f"Api Exception {response.status_code}")
